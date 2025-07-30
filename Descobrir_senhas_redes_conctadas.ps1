f9a8b053e59c0b947c416857acf0e39236461f59
Luan B. S. C. <138558778+Luanqmata@users.noreply.github.com>
2025-07-30 15:11:16 -0300

    Create Descobrir_senhas_redes_conctadas.ps1

---
+++ Descobrir_senhas_redes_conctadas.ps1
@@ -0,0 +1,16 @@
+$profiles = netsh wlan show profiles | Select-String "Todos os Perfis de Usuário|All User Profile"
+
+foreach ($profile in $profiles) {
+    $ssid = ($profile.Line -split ":")[1].Trim()
+    $result = netsh wlan show profile name="$ssid" key=clear
+
+    $passwordLine = $result | Select-String "Conteúdo da Chave|Key Content"
+
+    if ($passwordLine) {
+        $password = ($passwordLine.Line -split ":")[1].Trim()
+    } else {
+        $password = "senha não encontrada"
+    }
+
+    Write-Output "SSID: $ssid | Senha: $password"
+}
